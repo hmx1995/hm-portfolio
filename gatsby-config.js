baf7dc9b124e0d3e17356c27ebebccb3ab6b8597
Mansour Jalaly <mansour.jalaly@kcl.ac.uk>
2019-07-21 14:30:40 +0100

    Edit Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Mansour Jalaly - Portfolio",
+    author: "Mansour Jalaly",
+    description: "Mansour Jalaly's portfolio showcasing talent"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
