bd8d3a4cfa4739a9662833b22ff4f76e6093120b
Mansour Jalaly <mansour.jalaly@kcl.ac.uk>
2019-08-03 17:17:22 +0100

    Edit Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'mansour-jalaly-portfolio',
+        short_name: 'portfolio',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/MJ-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
